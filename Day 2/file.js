c852b49e1c72888abdef052a2ec526b5a57b0cb9
Chhatrapati Chauhan <116446768+chhatrapati295@users.noreply.github.com>
2023-06-03 17:51:43 +0530

    Updated file.js

--- Day 2/file.js
+++ Day 2/file.js
@@ -1,21 +1,40 @@
+const QUOTE_URL = 'https://type.fit/api/quotes';
+
 const quote = document.querySelector('.quote')
 const author = document.querySelector('.author')
-const newQuote = document.querySelector('.btn2')
+const nextBtn = document.querySelector('.next')
+const twitterBtn = document.querySelector('.twitter')
+const loader = document.querySelector('.img')
 
 window.addEventListener('DOMContentLoaded',()=>{
     getQuoteData()
 })
+
 async function getQuoteData(){
-    try{
-        const url = await fetch('https://zenquotes.io/api/quotes')
-        const json = await url.json()
-        quote.textContent = json[Math.floor(Math.random()* json.length-1)].q
-        author.textContent = json[Math.floor(Math.random()* json.length-1)].a
-    }catch(err){
-        console.log(err)
-    }
+    loader.style.display = 'block';
+    const url = await fetch(QUOTE_URL)
+    const json = await url.json()
+    loader.style.display = 'none';
+    console.log(json[Math.floor(Math.random()* json.length-1)])
+    setQuoteData(json[Math.floor(Math.random()* json.length-1)])
 }
 
-newQuote.addEventListener('click',()=>{
+function setQuoteData(item){
+    quote.textContent = item.text
+    author.textContent = item.author
+}
+
+nextBtn.addEventListener('click',()=>{
+    quote.textContent = '';
+    author.textContent = ''
     getQuoteData()
 })
+
+twitterBtn.addEventListener('click',()=>{
+    makeTweet()
+})
+
+function makeTweet(){
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`
+    window.open(tweetUrl , '_blank')
+}
