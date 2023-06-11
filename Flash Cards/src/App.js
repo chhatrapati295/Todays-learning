af48fbb892716b55347db39e415145d67cc7c360
Chhatrapati Chauhan <116446768+chhatrapati295@users.noreply.github.com>
2023-06-11 12:48:23 +0530

    Flash card react initial

---
+++ Flash Cards/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import Paper from './components/Paper';
+// import Header from './components/Header';
+// import Forms from './components/Forms';
+// import Lists from './components/Lists';
+// import Stats from './components/Stats';
+const App = () => {
+  return (
+      <div className="app">
+        {/* <Header/>
+        <Forms/>
+        <Lists/>
+        <Stats/> */}
+        <Paper/>
+      </div>
+  );
+}
+
+export default App
